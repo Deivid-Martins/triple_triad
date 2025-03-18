5356afaaa60b2c394404d58a72b5d88749ae4aed
Deivid Martins <deividrocha2005@gmail.com>
2025-03-18 11:47:28 -0300

    feat: add Player class on domain package and add initial functions inside this

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import domain.Card;
+import domain.Player;
 import utils.Tool;
 
 import java.util.ArrayList;
@@ -6,14 +7,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Card card1 = new Card("Charizard", 2, 9, 5, 3);
-        Card card2 = new Card("Vulva", 4, 3, 5, 3);
-        Card card3 = new Card("Squirt", 6, 2, 1, 9);
-        List<Card> cards = new ArrayList<>();
-        cards.add(card1);
-        cards.add(card2);
-        cards.add(card3);
-
-        Tool.printCardsArray(cards.toArray(new Card[0])); // Sim, o tamanho é 0 inicialmente, não se preocupe com isso
+        System.err.println("Main is empty");
     }
 }
---
+++ src/domain/Player.java
@@ -0,0 +1,42 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Player {
+    private String name;
+    public List<Card> cards = new ArrayList<Card>();
+
+    public Player(String name, Card[] cards) {
+        this.name = name;
+        this.cards = List.of(cards);
+    }
+
+    public Player(String name, List<Card> cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Name: ");
+        this.name = input.nextLine();
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public void printPlayerInfo() {
+        System.out.println("Player name: " + this.name);
+        System.out.println("Quantity of cards: " + this.cards.size());
+        for (Card card : this.cards) {
+            card.printCard();
+        }
+    }
+}
