e082f53260ded0e7245c8a7a5b1e16b092d7fbe2
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-04-07 18:06:19 -0300

    style: adding 'StartGame' javaDocs and comments

--- src/configuration/StartGame.java
+++ src/configuration/StartGame.java
@@ -6,36 +6,47 @@ import utils.Tool;
 
 import java.util.Scanner;
 
+/**
+ * This class is used to, literally, start the game. It's useful to clear the 'Main' class.
+ */
 public class StartGame {
+    // class constant declarations
     public static final Scanner inputText = new Scanner(System.in); // Input to text
     public static final Scanner inputNum = new Scanner(System.in); // Input to Numbers (int, float, ...)
 
-    public final CardsLibrary cardsLibrary;
-    public final Gameboard gameboard;
-    public Player playerOne, playerTwo;
+    // class declarations
+    public final CardsLibrary cardsLibrary; // all cards that can be played
+    public final Gameboard gameboard; // literally the gameboard
+    public Player playerOne, playerTwo; // the players that will play the game
 
+    /**
+     * StartGame inplict constructor, intanciating a board and a library
+     */
     public StartGame() {
         cardsLibrary = new CardsLibrary();
         gameboard = new Gameboard();
     }
 
+    /**
+     * Starts the game
+     */
     public void execute() {
-        System.out.println("Bem vindo ao jogo Triple Triad!");
-        System.out.println("Por favor, insira o nome dos jogadores");
+        System.out.println("Wellcome to the Triple Triad! game");
+        System.out.println("Please, enter the player's name!");
 
-        System.out.print("Jogador 1: ");
+        System.out.print("Player 1: ");
         String aux = inputText.nextLine();
         this.playerOne = new Player(aux, true);
 
-        System.out.print("Jogador 2: ");
+        System.out.print("Player 2: ");
         aux = inputText.nextLine();
         this.playerTwo = new Player(aux, false);
 
-        System.out.println("Nomes definidos com sucesso!");
-        System.out.println("Iremos sortear 5 cartas para cada um agora...");
+        System.out.println("Names defineds with sucess!");
+        System.out.println("We'll randomly choose 5 cards to the players now...");
         this.playerOne.setCardsArray(cardsLibrary.getRandomCards(5));
         this.playerTwo.setCardsArray(cardsLibrary.getRandomCards(5));
-        System.out.println("Cartas entregadas com sucesso!");
+        System.out.println("Cards choosen with sucess!");
 
         gameboard.showBoard();
         playerMenu(playerOne, playerTwo);
@@ -43,18 +54,23 @@ public class StartGame {
         gameboard.showBoard();
         playerMenu(playerTwo, playerOne);
     }
-    
+
+    /**
+     * The options menu that the player has when he will play his turn
+     * @param playerOnTurn wich player is playing right now
+     * @param playerOponent wich player is waiting to play
+     */
     private void playerMenu(Player playerOnTurn, Player playerOponent) {
-        System.out.println(playerOnTurn.getName() + ", oque quer fazer?");
+        System.out.println(playerOnTurn.getName() + ", what do you want to do?");
         int choice = 0;
         do {
-            System.out.println("1 - Fazer minha jogada");
-            System.out.println("2 - Ver minhas cartas");
-            System.out.println("3 - Ver cartas do oponente");
-            System.out.println("4 - Ver minha pontuação");
-            System.out.println("5 - Ver Tabuleiro\n");
+            System.out.println("1 - Play a card on the table");
+            System.out.println("2 - See my cards");
+            System.out.println("3 - See oponent's cards");
+            System.out.println("4 - See my pontuation");
+            System.out.println("5 - See the game board\n");
 
-            System.out.print("Faça sua escolha: ");
+            System.out.print("Make your choice: ");
 
             choice = Tool.nextIntLim(inputNum, 1, 5);
 
@@ -69,40 +85,45 @@ public class StartGame {
                     playerOponent.printAllCards();
                     break;
                 case 4:
-                    System.out.println("Pontuação de " + playerOnTurn.getName() + ": " + playerOnTurn.getPoints());
+                    System.out.println(playerOnTurn.getName() + " point's: " + playerOnTurn.getPoints());
                     break;
                 case 5:
                     gameboard.showBoard();
                     break;
                 default:
-                    System.err.println("Opção inválida, tente novamente");
+                    System.err.println("Invalid Option, try again!");
                     break;
             }
         } while (choice != 1);
     }
 
+    /**
+     * This method starts when a player start your turn. This method let the player play.
+     * @param player wich player will play right now
+     */
     private void playerTurn(Player player) {
         gameboard.showBoard();
         player.printAllCards();
 
-        System.out.print("Defina a carta a jogar, escolha de 1 a " + player.getCards().size() + ": ");
+        System.out.print("Choose a card to play. Choose beetwen 1 and " + player.getCards().size() + ": ");
         int cardIndex = Tool.nextIntLim(inputNum, 1, player.getCards().size());
 
-        System.out.print("Defina a linha que deseja colocar a carta (0 a 2): ");
-        int i = Tool.nextIntLim(inputNum, 0, 2);
+        System.out.print("Choose a line to put the card (0 a 2): ");
+        int line = Tool.nextIntLim(inputNum, 0, 2);
 
-        System.out.print("Defina a coluna que deseja colocar a carta (0 a 2): ");
-        int j = Tool.nextIntLim(inputNum, 0, 2);
+        System.out.print("Choose a column to put the card (0 a 2): ");
+        int column = Tool.nextIntLim(inputNum, 0, 2);
 
-        while(gameboard.getMatriz()[i][j] != null) {
-            System.err.println("Celula preenhida, tente novamente");
+        // enquanto ele colocar num lugar que tenha carta
+        while(gameboard.getMatriz()[line][column] != null) {
+            System.err.println("It's alredy has a card here, try again with a different position!");
 
-            System.out.print("Defina a linha que deseja colocar a carta (1 a 3): ");
-            i = Tool.nextIntLim(inputNum, 0, 2);
+            System.out.print("Choose a line to put the card (0 a 2): ");
+            line = Tool.nextIntLim(inputNum, 0, 2);
 
-            System.out.print("Defina a coluna que deseja colocar a carta (1 a 3): ");
-            j = Tool.nextIntLim(inputNum, 0, 2);
+            System.out.print("Choose a column to put the card (0 a 2): ");
+            column = Tool.nextIntLim(inputNum, 0, 2);
         }
-        player.setPoints(gameboard.addCard(i, j, player.getCards().get(cardIndex - 1)));
+        player.setPoints(gameboard.addCard(line, column, player.getCards().get(cardIndex - 1)));
     }
 }
