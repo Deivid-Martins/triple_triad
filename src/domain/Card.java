38c936ae6eb2cfd3c27cee210acdd8dcbcd36342
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-04-07 12:22:57 -0300

    style: adding 'Card' javaDocs and comments

--- src/domain/Card.java
+++ src/domain/Card.java
@@ -3,10 +3,21 @@ package domain;
 import java.util.Scanner;
 
 public class Card {
-    private int up, down; // eixo y
-    private int left, right; // eixo X
-    private Player owner;
+    // class declarations
+    private Player owner; // who is the owner of this card (player 1 or 2)
 
+    //primitive declarations
+    private int up, down; // Y axis
+    private int left, right; // X axis
+
+    /**
+     * Regular constructor to the card
+     * @param up up valor
+     * @param down down valor
+     * @param left left valor
+     * @param right right valor
+     * @param owner owner of this card
+     */
     public Card(int up, int down, int left, int right, Player owner) {
         this.up = up;
         this.down = down;
@@ -15,6 +26,13 @@ public class Card {
         this.owner = owner;
     }
 
+    /**
+     * Constructor to the card without the presence of the owner. Means that this card has no owner yet
+     * @param up up valor
+     * @param down down valor
+     * @param left left valor
+     * @param right right valor
+     */
     public Card(int up, int down, int left, int right) {
         this.up = up;
         this.down = down;
@@ -22,6 +40,39 @@ public class Card {
         this.right = right;
     }
 
+    /**
+     * Print the card object in terminal
+     */
+    public void print() {
+        System.out.println("+-----+\n" +
+                             "|  "+ (up == 10 ? "A" : up) +"  |\n" +
+                             "| "+ (left == 10 ? "A" : left) +" "+ (right == 10 ? "A" : right) +" |\n" +
+                             "|  "+ (down == 10 ? "A" : down) +"  |\n" +
+                             "+-----+");
+    }
+
+    /**
+     * Gives the card information
+     * @return return card parameters
+     */
+    @Override
+    public String toString() {
+        return "Card{" +
+                "owner=" + owner.getName() +
+                ", up=" + up +
+                ", down=" + down +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+
+    //General Setters
+    public void setOwner(Player owner) {
+        if(owner!=null)
+            this.owner = owner;
+    }
+
+    //General Getters
     public int getUp() {
         return up;
     }
@@ -38,26 +89,5 @@ public class Card {
         return right;
     }
 
-    public Player getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Player owner) {
-        this.owner = owner;
-    }
-
-    public void print() {
-        System.out.println("+-----+\n" +
-                             "|  "+ (up == 10 ? "A" : up) +"  |\n" +
-                             "| "+ (left == 10 ? "A" : left) +" "+ (right == 10 ? "A" : right) +" |\n" +
-                             "|  "+ (down == 10 ? "A" : down) +"  |\n" +
-                             "+-----+");
-    }
-
-    @Override
-    public String toString() {
-        return "Card{" +
-                "owner=" + owner.getName() +
-                '}';
-    }
+    public Player getOwner() { return owner;}
 }
