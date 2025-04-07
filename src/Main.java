b4106df7b5b21da90d42c44d8b4f2c1b1e00dcf6
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-04-07 18:07:25 -0300

    style: adding 'Main' javaDocs and comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import configuration.StartGame;
 
+/**
+ * Class that runs the game to test the code
+ */
 public class Main {
     public static void main(String[] args) {
         StartGame startGame = new StartGame();
