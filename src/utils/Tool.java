0f1f5169975747e7fdb166e5b971e3f39c5e35e4
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-04-07 17:48:47 -0300

    style: adding 'Tool' javaDocs and comments

--- src/utils/Tool.java
+++ src/utils/Tool.java
@@ -2,7 +2,17 @@ package utils;
 
 import java.util.Scanner;
 
+/**
+ * Tool class is a general use class. It's very useful to store a lor of methods that could be used without a specific case.
+ */
 public abstract class Tool {
+    /**
+     * Method to limit the input of and integer
+     * @param input Scanner that will be used
+     * @param min int minimun number
+     * @param max int maximun number
+     * @return an int beetwen min and max
+     */
     public static int nextIntLim(Scanner input, int min, int max) {
         int x = input.nextInt();
 
